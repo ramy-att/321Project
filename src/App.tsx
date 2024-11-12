fe863dbca160c5b876dc95b0718be71aff8141b7
Ramy Attalla <ramysamerattalla@gmail.com>
2024-11-11 21:44:05 -0500

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const scanPage = () => {
 
+  }
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Policy Analyzer</h1>
+      <p>
+        The extension read your page, finds the privacy policy and analyzes it.
       </p>
+      <div className="card">
+        <button onClick={scanPage}>Scan Page</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
